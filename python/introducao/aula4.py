08d0f90db4668e19cde2268f3e1a50b17bf395d5
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-12-01 16:03:10 -0300

    fim aula 04

--- python/introducao/aula4.py
+++ python/introducao/aula4.py
@@ -1,14 +1,57 @@
-a = int(input('Entre com o número: '))
+a = int(input('Primeiro bimestre: '))
+while a > 10:
+    a = int(input('Você digitou errado. Primeiro bimestre: '))
+b = int(input('Segundo bimestre: '))
+while b > 10:
+    b = int(input('Você digitou errado. Segundo bimestre: '))
+c = int(input('Terceiro bimestre: '))
+while c > 10:
+    c = int(input('Você digitou errado. Terceiro bimestre: '))
+d = int(input('Quarto bimestre: '))
+while d > 10:
+    d = int(input('Você digitou errado. Quarto bimestre: '))
 
-div = 0
+media  = (a + b + c + d)/4
 
-for x in range(1, a+1):
-    resto = a % x
-    print('{} dividido por {} o resto é igual {}'.format(a, x, resto))
-    if resto == 0:
-        div += 1
+print('media: {}'.format(media))
 
-if div == 2:
-    print('número {} é primo'.format(a))
-else:
-    print('número {} não primo'.format(a))
+# nota = int(input('Entre com a nota: '))
+# while nota > 10:
+#     nota = int(input('Nota inválida. Entre com a nota: '))
+
+# a = 0
+
+# while a < 10:
+#     print(a)
+#     a += 1
+
+
+# a = int(input('Entre com um valor: '))
+
+# for num in range(a+1):   
+
+#     div = 0
+
+#     for x in range(1, num+1):
+#         resto = num % x
+#         # print('{} dividido por {} o resto é igual {}'.format(num, x, resto))
+#         if resto == 0:
+#             div += 1
+
+#     if div == 2:
+#         print(num)
+
+# a = int(input('Entre com o número: '))
+
+# div = 0
+
+# for x in range(1, a+1):
+#     resto = a % x
+#     print('{} dividido por {} o resto é igual {}'.format(a, x, resto))
+#     if resto == 0:
+#         div += 1
+
+# if div == 2:
+#     print('número {} é primo'.format(a))
+# else:
+#     print('número {} não primo'.format(a))
