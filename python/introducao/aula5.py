1e285df4116bdcc247dd63a259ce86498cf9f75d
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-12-04 16:03:30 -0300

    aula 5

---
+++ python/introducao/aula5.py
@@ -0,0 +1,44 @@
+lista = [1, 3, 5, 7]
+lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+
+tupla = (1, 10, 12, 14)
+print(tupla)
+
+tupla_animal = tuple(lista_animal)
+
+print(tupla_animal)
+
+lista_numerica = list(tupla)
+print(lista_numerica)
+
+# lista.sort()
+# lista_animal.sort()
+
+# print(lista)
+# print(lista_animal)
+
+# lista_animal.reverse()
+# print(lista_animal)
+# nova_lista = lista_animal * 3
+
+# print(lista_animal[1])
+# print(sum(lista))
+# print(max(lista))
+# print(min(lista))
+
+# print(max(lista_animal))
+# print(min(lista_animal))
+
+# if 'lobo' in lista_animal:
+#     print('existe um gato na lista')
+# else:
+#     print('não existe um lobo na lista')
+#     lista_animal.append('lobo')
+
+# print(lista_animal)
+
+# lista_animal.pop(0)
+# print(lista_animal)
+
+# lista_animal.remove('elefante')
+# print(lista_animal)
