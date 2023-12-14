5111f72b3830d903502650e834b89a483538bdaf
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-12-14 16:45:57 -0300

    convertendo tipos

---
+++ python/conhecendo_a_linguagem/convertendo_tipos.py
@@ -0,0 +1,13 @@
+print(int(1.9))
+
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+valor = 10
+valor_str = '10'
+
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2)
+print(100 // 2)
