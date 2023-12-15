6074bbac0aaa0c104517a7a4bff01fdc5bd0c321
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-12-15 13:24:48 -0300

    fim

---
+++ python/conhecendo_a_linguagem/print_input.py
@@ -0,0 +1,8 @@
+nome = input('Informe o seu nome: ')
+idade = input('Informe a sua idade: ')
+
+
+print(nome, idade,)
+print('teste', end=' ')
+print(nome, idade, end='...\n')
+print(nome, idade, sep='#')
