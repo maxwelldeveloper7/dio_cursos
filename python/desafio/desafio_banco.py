6be080a95057a3a96472344870cef7f6d8ed9b4a
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-12-21 16:43:36 -0300

    desafio banco

--- python/desafio/desafio_banco.py
+++ python/desafio/desafio_banco.py
@@ -1,27 +1,28 @@
+menu = """
+========== MENU OPERAÇÕES BANCÁRIAS ===========
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+=> """
 
-LIMITE_DE_SAQUES = 3
-VALOR_MAXIMO_DIARIO_SAQUE = 500.00
-saldo = 0.0
+saldo = 0
+limite = 500
 extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
 
 while True:
-    print("""
-    ========== MENU OPERAÇÕES BANCÁRIAS ===========
-    [1] para depósito
-    [2] para saque
-    [3] para extrato
-    [0] para sair
-    """)
-    opcao = input('Escola uma oção: ')
+    opcao = input(menu)
     
-    if opcao == '1':
+    if opcao == 'd':
         print('Depósito')
-    elif opcao == '2':
+    elif opcao == 's':
         print('Saque')
-    elif opcao == '3':
+    elif opcao == 'e':
         print('Extrato')
-    elif opcao == '0':
+    elif opcao == 'q':
         print('Fim do programa')
         break
     else:
-        print('Oção inválida')
+        print('Oção inválida, por favor selecione novamente a operação desejada.')
