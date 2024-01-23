8a16f4a4fc4932b7cc0e3d73f18dba6fe0a57e95
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-01-23 12:54:11 -0300

    estilizando a app principal

--- chatgpt-clone/web/src/App.js
+++ chatgpt-clone/web/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import { makeRequest } from './api/api';
+
+import './styles/reset.css';
 import './styles/App.css';
 
+import { SideMenu } from './components/SideMenu/SideMenu';
+import { ChatMessage } from './components/ChatMessage/ChatMessage';
+
+
 function App() {
+
+  const[input, setInput] = useState("");
+  const[chatlog, setChatlog] = useState([{user:"gpt", message:"Como posso te ajudar hoje?"}]);
+
+  async function handleSubmit(e){
+      e.preventDefautl()
+      let response = await makeRequest({prompt: input})
+      response = response.data.split('\n').map(line => <p>{line}</p>);
+      setChatlog([...chatlog,{user:'me', message: `${input}`},{user:'gpt',message: response}]);
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideMenu></SideMenu>
+      <section className='chatbox'>
+        <div className='caht-log'>
+          {chatlog.map((message, index)=>(
+            <ChatMessage
+              key = {index}
+              message = {message}
+            />
+          ))}
+        </div>
+        <div className='chat-input-holder'>
+            <form onSubmit={handleSubmit}>
+              <input rows='1' className='chat-input-textarea' value={input} onChange={e => setInput(e.target.value)}/>
+            </form>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ chatgpt-clone/web/src/components/ChatMessage/ChatMessage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import  './ChatMessage.css';
+import Avatar from "../../assets/avatar";
+
+export const ChatMessage = ({message})=>{
+    <div className={`chat-message ${message.user === 'gpt'}  && "chatgpt"`}>
+        <div className="chat-message-center">
+            <div className={`avatar ${message.user === 'gpt' && "chatgpt"}`}>
+                {message.user === 'gpt' && (<Avatar/>)}
+            </div>
+        </div>
+        <div className="message">
+            {message.message}
+        </div>
+    </div>
+}
