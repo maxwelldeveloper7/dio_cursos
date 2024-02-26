2da1fd2872d47d18547a678cf03443a5e2d0ca67
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-02-25 22:15:46 -0300

    criando entidades parte 2

--- python/integration_with_sqlalchemy/integrationWithSQL/sqlAlchemyApplication.py
+++ python/integration_with_sqlalchemy/integrationWithSQL/sqlAlchemyApplication.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
@@ -9,4 +10,17 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user_account"
     # atributos
-    id = Column(Integer)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    fullname = Column(String)
+    
+    address = relationship(
+        "Address", back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class Address(Base):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email_address = Column(String(30), nullable=False)
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
+
