0d7301f61111138328c35f4d40dbe47fda846652
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-04-09 13:59:24 -0300

    aula 1

---
+++ python/desenvolvimento_web/ola_flask/run.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/<numero>', methods=['GET','POST'])
+def ola(numero):
+    return 'Olá Mundo! {}'.format(numero)
+
+if __name__ == '__main__':
+    app.run(debug=True)
