804146edcb93c6c89d4b4824a71675a826e5f4e9
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-04-30 10:11:37 -0300

    implementação da tarefa

--- python/desenvolvimento_web/dev_api/app_rest.py
+++ python/desenvolvimento_web/dev_api/app_rest.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from habilidades import Habilidades
+from habilidades import Habilidades, ListaHabilidades
 import json
 
 app = Flask(__name__)
@@ -53,7 +53,8 @@ class ListaDesenvolvedores(Resource):
 
 api.add_resource(Desenvolvedor, '/dev/<int:id>/')
 api.add_resource(ListaDesenvolvedores, '/dev/')
-api.add_resource(Habilidades, '/habilidades/')
+api.add_resource(ListaHabilidades, '/habilidades/')
+api.add_resource(Habilidades, '/habilidade/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- python/desenvolvimento_web/dev_api/habilidades.py
+++ python/desenvolvimento_web/dev_api/habilidades.py
@@ -1,7 +1,31 @@
+import json
 from flask_restful import Resource
+from flask import request
 
 lista_habilidades = ['Python', 'Java', 'Flask', 'PHP']
 
 class Habilidades(Resource):
+    def get(self, id):
+        try:
+            response = lista_habilidades[id]
+        except IndexError:
+            mensagem = 'Habilidade de ID {} não existe'
+            response = {'status':'erro', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status':'erro', 'mensagem':mensagem}
+        print(response)
+        return response
+
+    def put(self, id):
+        dados = json.loads(request.data)
+        lista_habilidades[id] = dados
+        return dados
+
+    def delete(self, id):
+        lista_habilidades.pop(id)
+        return {'status':'sucesso', 'mensagem':'Registro excluído'}
+
+class ListaHabilidades(Resource):
     def get(self):
         return lista_habilidades
