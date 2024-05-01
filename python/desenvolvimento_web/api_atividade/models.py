4fd1212dd085b44d37a5ce27477397e14352ac46
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-05-01 11:06:48 -0300

    Manipulando banco de dados com SQLAlchemy

---
+++ python/desenvolvimento_web/api_atividade/models.py
@@ -0,0 +1,44 @@
+""" Implementa modelos e conexão e sessão com banco de dados."""
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.orm import declarative_base
+
+engine = create_engine('sqlite:///atividades.db')
+db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Pessoas(Base):
+    """ Base para criação da tabela pessoas."""
+    __tablename__ = 'pessoas'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    idade = Column(Integer)
+
+    def __repr__(self):
+        return f'<Pessoa {self.nome}>'
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+class Atividades(Base):
+    """ Base para criação da tabela atividades"""
+    __tablename__ = 'atividades'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
+    pessoa = relationship("Pessoas")
+
+def init_db():
+    """ Cria o banco de dados"""
+    Base.metadata.create_all(bind=engine)
+
+
+if __name__ == "__main__":
+    init_db()
---
+++ python/desenvolvimento_web/api_atividade/utils.py
@@ -0,0 +1,32 @@
+""" Realiza testes de persistência no banco de dados atividades.db"""
+from models import Pessoas
+
+def insere_pessoas():
+    """ Insere dados na tabela pessoa. """
+    pessoa = Pessoas(nome='Chaves', idade=45)
+    print(pessoa)
+    pessoa.save()
+
+def consulta():
+    """ Consulta dados na tabela pessoa. """
+    #pessoa = Pessoas.query.all()
+    pessoa = Pessoas.query.filter_by(nome='Maxwell')
+    for p in pessoa:
+        print(f'Nome: {p.nome}', end=" - ")
+        print(f'Idade: {p.idade}')
+
+def altera_pessoa():
+    """ Altera dados na tabela pessoa. """
+    pessoa = Pessoas.query.filter_by(nome='Maxwell').first()
+    pessoa.idade = 44
+    pessoa.save()
+
+def exclui_pessoa():
+    """ Deleta dados na tabela pessoa. """
+    pessoa = Pessoas.query.filter_by(nome='Chaves').first()
+    pessoa.delete()
+
+if __name__ == '__main__':
+    #insere_pessoas()
+    exclui_pessoa()
+    consulta()
