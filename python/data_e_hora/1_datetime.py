cef07d18730a6d4898bc365288c2c206d7208398
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-05-30 21:35:59 -0300

    date, datetime e time

--- python/data_e_hora/1_datetime.py
+++ python/data_e_hora/1_datetime.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date, datetime, time
 
 data = date(2023, 7, 10)
 print(data)
@@ -7,3 +7,6 @@ print(data.today())
 data_hora = datetime(2023, 7, 10, 20, 30, 40)
 print(data_hora)
 print(data_hora.today())
+
+hora = time(20, 30, 40)
+print(hora)
