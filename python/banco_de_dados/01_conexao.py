9e8ac7fdcaea3a053ab52b173c06112e52ebf39e
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-06-11 10:54:28 -0300

    inserindo registros

--- python/banco_de_dados/01_conexao.py
+++ python/banco_de_dados/01_conexao.py
@@ -6,4 +6,8 @@ ROOT_PATH = Path(__file__).parent
 conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")
 cursor = conexao.cursor()
 
-cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
+# cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
+
+data = ('Maria', 'maria@maria')
+cursor.execute('INSERT INTO clientes (nome, email) VALUES (?, ?);', data)
+conexao.commit()
