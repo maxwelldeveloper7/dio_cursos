4d2bff1914a1f942d39eb37a55b718a4008204a8
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-06-11 09:13:27 -0300

    criando uma tabela

--- python/banco_de_dados/01_conexao.py
+++ python/banco_de_dados/01_conexao.py
@@ -3,5 +3,7 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-conexao = sqlite3.connect(ROOT_PATH / "clientes.db")
-print(conexao)
+conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")
+cursor = conexao.cursor()
+
+cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
