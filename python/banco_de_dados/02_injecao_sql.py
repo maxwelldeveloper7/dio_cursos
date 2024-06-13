4d772b8d228a444d584f7564ba11e2ea2c88347c
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-06-12 23:09:15 -0300

    boas praticas

--- python/banco_de_dados/02_injecao_sql.py
+++ python/banco_de_dados/02_injecao_sql.py
@@ -7,7 +7,7 @@ conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")
 cursor = conexao.cursor()
 cursor.row_factory =  sqlite3.Row
 
-id_cliente = input("Digite o ID do cliente: ")
+id_cliente = input("Digite o ID do cliente: ") # informe: 1 or 1=1; | exibirá todos os registros
 cursor.execute(f"SELECT * FROM clientes WHERE id = {id_cliente}")
 clientes = cursor.fetchall()
 for cliente in clientes:
