d37e77ce9b808417a6edcb08cdc658f5214973c6
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-07-01 16:08:16 -0300

    exercícios

--- javascript/primeiros_passos/listas/exercicios/imprimirPar.js
+++ javascript/primeiros_passos/listas/exercicios/imprimirPar.js
@@ -7,7 +7,7 @@ for (let i = 0; i < 100; i++) {
 console.log('Mostrando números pares de 1 a 100')
 
 for (let i = 0; i < 100; i++) {
-    if(i % 2 == 0){
+    if(i % 2 === 0){
         console.log(`${i}`);
     }
 }
