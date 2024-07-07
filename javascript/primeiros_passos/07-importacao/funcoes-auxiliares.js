dee5927cb7bffba5598bc70dedf335f02f6dad75
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-07-07 20:19:04 -0300

    Praticando os conteúdos de importação e exportação

--- javascript/primeiros_passos/07-importacao/funcoes-auxiliares.js
+++ javascript/primeiros_passos/07-importacao/funcoes-auxiliares.js
@@ -1,5 +1,10 @@
+const entradas = [10,5, 50, 10, 98, 23, 33, 44, 99, 1, 7]
+let i = 0;
+
 function gets(){
-    return 10;
+    const valor = entradas[i];
+    i++;
+    return valor;
 }
 
 function print(texto){
--- javascript/primeiros_passos/07-importacao/main.js
+++ javascript/primeiros_passos/07-importacao/main.js
@@ -1,10 +1,39 @@
-const funcoes = require('./funcoes-auxiliares');
+const { gets, print } = require('./funcoes-auxiliares');
 
-funcoes.print('oi');
-console.log(funcoes.gets());
+const quantidadeDeAlunos = gets();
+let maiorValor = 0;
 
-// Object Destructuring
-const {gets, print} = require('./funcoes-auxiliares');
+for (let i = 0; i < quantidadeDeAlunos; i++) {
+    const numerosSorteado = gets();    
+    if (numerosSorteado > maiorValor) {
+        maiorValor = numerosSorteado;
+    }
+}
 
-print('oi');
-console.log(gets());
+print(maiorValor);
+
+/*
+
+    Uma salda contem 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+    Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+    Dados de entrada:
+    5
+    50
+    10
+    98
+    23
+
+    Saída:
+    98
+
+*/
+//const funcoes = require('./funcoes-auxiliares');
+// funcoes.print('oi');
+// console.log(funcoes.gets());
+
+// // Object Destructuring
+// const {gets, print} = require('./funcoes-auxiliares');
+
+// print('oi');
+// console.log(gets());
