e13f44903acb5b510625afb5101725e4d983d5d6
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-08-24 21:00:48 -0300

    parte 2

---
+++ typescript/intro_react/my-app-ts/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card } from "./components/Card";
+
+function App() {
+  return (
+    <React.Fragment>
+    <div>
+      Hello word
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    <div>
+      Footer
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ typescript/intro_react/my-app-ts/src/components/Card.tsx
@@ -0,0 +1,8 @@
+export const Card = () => {
+    return (
+        <div className="card">
+            <h1>Card</h1>
+            <p>Parágrafo</p>
+        </div>
+    )
+}
