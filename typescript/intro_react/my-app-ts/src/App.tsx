a34170ab0eabe51434eaa12790c1a3e802f53697
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-09-15 20:56:13 -0300

    Estruturando a página

--- typescript/intro_react/my-app-ts/src/App.tsx
+++ typescript/intro_react/my-app-ts/src/App.tsx
@@ -1,15 +1,25 @@
-import { Layout } from "./components/Layout";
-import styled from "styled-components";
-import { Center, ChakraProvider, Input } from "@chakra-ui/react";
+
+import { Box, Button, Center, ChakraProvider, Input } from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
-      <Center>
-        <h1>Faça o login</h1>      
+      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+        <Box backgroundColor='#ffffff' borderRadius='25px' padding='15px'>
+        <Center>
+          <h1>Faça o login</h1>      
+        </Center>
         <Input placeholder="email" />
-        <Input placeholder="password" />    
-      </Center>          
+        <Input placeholder="password" />
+        <Center>
+        <Button colorScheme="teal" size='sm' width='100%'>
+          Button
+        </Button>
+        </Center>
+        </Box>
+      </Box>
+      
+                
     </ChakraProvider>
   );
 }
