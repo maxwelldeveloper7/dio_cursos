30a13477909a69d1ab6741b41ad1f9a3cbde5d30
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-09-19 12:37:28 -0300

    Trabalhando com estado

--- typescript/avancado_react/src/App.tsx
+++ typescript/avancado_react/src/App.tsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import { Card } from "./components/Card";
 import { Layout } from "./components/Layout";
 
-
 function App() {
+  const [value, setValue] = useState(0);
+  const [outroValor, setOutroValor] = useState(1)
   return (
     <ChakraProvider>
-        <Layout>
-          <Box minHeight='100vh' backgroundColor='#9413dc' padding="25px">
-            <Card /> 
-          </Box>
-        </Layout>
+      <Layout>
+        <Box minHeight="100vh" backgroundColor="#9413dc" padding="25px">
+          <Card />
+          <div>
+            <button onClick={()=> setValue(value + 1)}>Add</button>
+            <h1>{value}</h1>
+          </div>
+            <button onClick={()=> setOutroValor(outroValor + 1)}>Add</button>
+            <h1>{outroValor}</h1>          
+        </Box>
+      </Layout>
     </ChakraProvider>
-
   );
 }
 
